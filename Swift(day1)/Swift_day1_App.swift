6b7db818476a9d6c7e19a2e66de1d75b924d62d2
nke <nke@nke.ru>
2023-02-12 12:34:15 +0700

    Initial Commit

---
+++ Swift(day1)/Swift_day1_App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_day1_App.swift
+//  Swift(day1)
+//
+//  Created by admin on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_day1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
