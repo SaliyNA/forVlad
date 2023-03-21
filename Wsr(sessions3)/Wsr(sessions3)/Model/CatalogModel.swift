d7a35ffcdf8e408a589c17ae56ef611ca93dd9cc
nke <nke@nke.ru>
2023-03-21 22:41:53 +0700

    session3_2

---
+++ Wsr(sessions3)/Wsr(sessions3)/Model/CatalogModel.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogModel.swift
+//  Wsr(sessions3)
+//
+//  Created by admin on 21.03.2023.
+//
+
+import Foundation
+struct CatalogModel: Codable, Hashable{
+    var id: Int
+    var name: String
+    var description: String
+    var price: String
+    var category: String
+    var time_result: String
+    var preparation: String
+    var bio: String
+}
